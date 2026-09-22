a0af0f3678a7dfff59c15ed58d8845b5fb0ddec7
Gustavo Tessaro <gu.tessaro@hotmail.com>
2026-09-22 18:12:24 -0300

    Adiciona aplicação do módulo de pedidos

--- src/Api/Compartilhado/Auth/UserProvider.cs
+++ src/Api/Compartilhado/Auth/UserProvider.cs
@@ -28,4 +28,10 @@ public sealed class UserProvider(IHttpContextAccessor httpContextAccessor) : IPr
 
     public bool EstaAutenticado => Id.HasValue;
 
+    public bool PossuiTipo(TipoUsuario tipoUsuario)
+    {
+        ClaimsPrincipal? user = httpContextAccessor.HttpContext?.User;
+
+        return user?.Identity?.IsAuthenticated == true && user.IsInRole(tipoUsuario.ToString());
+    }
 }
